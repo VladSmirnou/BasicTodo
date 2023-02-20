0dd75b7c04762e0ba4d8b20df022f74e88fc273b
VladSmirnou <smr.vladislav@gmail.com>
2023-02-20 17:07:50 +0300

    delete post block

--- basic_todo/views.py
+++ basic_todo/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from .forms import CustomUserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout
+from .models import UserPost
+from django.http import HttpResponse
+from django.contrib import messages
 
 # Even tho gunicorn pre-forks processes i still can add multiple threads 
 # per one process, so it's better to use class-based view, cus at least Django 
@@ -10,7 +13,8 @@ from django.contrib.auth import login, logout
 # their dictionaries data
 
 def home_page(request):
-    return render(request, 'home.html')
+    user_posts = UserPost.objects.all()
+    return render(request, 'home.html', {'user_posts': user_posts})
 
 
 class CreateUser(View):
@@ -47,6 +51,21 @@ class LoginUser(View):
         return render(request, self.template_name, {'form': form})
 
 
+class DeletePost(View):
+    def delete(self, request, pk):
+        try:
+            post = UserPost.objects.get(post_author=request.user.pk, pk=pk)
+            messages.success(request, f'Your post *{post.post_title}* was successfully deleted!')
+            post.delete()
+        except:
+            messages.error(request, 'Forbidden')
+        return redirect('home_page')
+
+
 def user_logout(request):
     logout(request)
     return redirect('home_page')
+
+
+def flash_message_cleaner(request):
+    return HttpResponse('')
