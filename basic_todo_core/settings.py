4e9da8326d9f61bb655a666082130c7a394e4745
VladSmirnou <smr.vladislav@gmail.com>
2023-02-21 18:03:50 +0300

    changes

--- basic_todo_core/settings.py
+++ basic_todo_core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'basic_todo.apps.BasicTodoConfig',
-    'django_htmx',
 ]
 
 MIDDLEWARE = [
@@ -53,8 +52,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_htmx.middleware.HtmxMiddleware',
-    'basic_todo.custom_middleware.SimpleMiddleware',
 ]
 
 ROOT_URLCONF = 'basic_todo_core.urls'
